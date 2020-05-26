75085c81e65dce6dd1b46cfd1a7a6486152b6e46
The-Verdugo <56098355+The-Verdugo@users.noreply.github.com>
2020-05-26 11:08:48 -0600

    archivos de consulta para auditado

---
+++ consulta_acta_auditado.php
@@ -0,0 +1,19 @@
+<?php
+include 'conexion.php';
+$id=$_GET['id'];
+
+
+$consulta ="SELECT * FROM acta 
+INNER JOIN persona ON acta.fk_id_auditor = persona.id_persona 
+INNER JOIN area ON acta.fk_id_area=area.id_area
+INNER JOIN departamento on acta.fk_id_departamento= departamento.id_departamento
+INNER JOIN status on acta.fk_id_status = status.id_status
+where acta.fk_id_persona=$id";
+$resultado = $conexion -> query($consulta);
+
+while($fila=$resultado->fetch_array()){
+	$persona[] = array_map('utf8_encode',$fila);
+}
+echo json_encode($persona);
+$resultado -> close();
+?>
